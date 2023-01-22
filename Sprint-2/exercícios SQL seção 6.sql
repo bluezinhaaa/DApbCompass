3d5225d73e70d47fff71678a3a3e22ac1fdef608
Bruna Jacom <brunajacom@gmail.com>
2023-01-22 20:51:11 -0300

    exercício 2

--- Sprint-2/exercícios SQL seção 6.sql
+++ Sprint-2/exercícios SQL seção 6.sql
@@ -37,4 +37,40 @@ FROM LIVRO L
 	ON L.AUTOR = A.CODAUTOR
 		INNER JOIN EDITORA ED
 		ON L.EDITORA = ED.CODEDITORA
-LIMIT 10;
+ORDER BY L.VALOR DESC LIMIT 10;
+
+
+/*
+Exportar o resultado da query que obtém as 5 editoras com maior 
+quantidade de livros na biblioteca para um arquivo CSV. Utilizar
+o caractere | (pipe) como separador. Lembre-se que o conteúdo 
+do seu arquivo deverá respeitar a sequência de colunas e seus 
+respectivos nomes de cabeçalho que listamos abaixo:
+
+CodEditora
+
+NomeEditora
+
+QuantidadeLivros
+
+Observação: O arquivo exportado, conforme as especificações acima, 
+deve ser disponibilizado no GitHub. Abaixo (na caixa de envio), 
+gentileza nos enviar o link do arquivo .csv que colocou no seu 
+github.
+exemplo de envio (resposta):
+Segue abaixo link do arquivo no formato .csv (referente a query 
+das 5 editoras com maior quantidade de livros na biblioteca) 
+conforme solicitado:
+https://github.com/aasouzaconsult/programabolsas/blob/main/arquivo2.csv
+*/
+
+SELECT ED.CODEDITORA AS CodEditora, 
+       ED.NOME AS NomeEditora,
+       COUNT(L.EDITORA) AS QuantidadeLivros
+FROM LIVRO L
+	INNER JOIN EDITORA ED
+	ON L.EDITORA = ED.CODEDITORA
+GROUP BY ED.CODEDITORA, 
+         ED.NOME,
+         L.EDITORA
+ORDER BY QuantidadeLivros DESC LIMIT 5;      
