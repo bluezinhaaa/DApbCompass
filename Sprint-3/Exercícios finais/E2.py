b6d3feffa13ebcd6aa2eb595e536fab25c325395
Bruna Jacom <brunajacom@gmail.com>
2023-01-25 11:15:14 -0300

    e2 da trilha

---
+++ Sprint-3/Exercícios finais/E2.py
@@ -0,0 +1,22 @@
+#Escreva um código Python que verifica se os números 0, 7851 e 9 
+# elevado na potência 3  são pares ou ímpares. Para cada número, 
+# imprima o Par: ou Ímpar: e o número correspondente.
+
+n1 = 0
+n2 = 7851
+n3 = 9 ** 3
+
+if (n1%2 == 0):
+    print('Par:' ,n1 )
+else:
+    print('Ímpar:' ,n1)
+
+if (n2%2 == 0):
+    print('Par:' ,n2 )
+else:
+    print('Ímpar:' ,n2)
+
+if (n3%2 == 0):
+    print('Par:' ,n3 )
+else:
+    print('Ímpar:' ,n3)
