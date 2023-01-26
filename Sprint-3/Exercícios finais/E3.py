609ab1db19fa35573a769d9c6ebfe6b4073d6721
Bruna Jacom <brunajacom@gmail.com>
2023-01-25 22:51:30 -0300

    e3 resolvido

---
+++ Sprint-3/Exercícios finais/E3.py
@@ -0,0 +1,7 @@
+#Escreva um código Python que imprime os números pares de 0 até 20 (incluso).
+#Dica: olhe a documentação da função range().
+
+for x in range(0, 21 + 1):
+
+    if x % 2 == 0: 
+        print(x, end = " ") 
