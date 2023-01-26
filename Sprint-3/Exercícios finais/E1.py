017573f4605e72b963035d7e5e528cefa5529acf
Bruna Jacom <brunajacom@gmail.com>
2023-01-25 23:05:15 -0300

    e1 resolvido

---
+++ Sprint-3/Exercícios finais/E1.py
@@ -0,0 +1,10 @@
+#Escreva um código Python que imprime o nome e a idade do João de 37 
+# anos e imprime o ano em que ele completará 100 anos.
+#Dica: você pode ler strings digitadas no teclado utilizando a função 
+# builtin input('mensagem'). Lembre-se de converter números para seu 
+# respectivo tipo (int ou float) antes fazer operações aritméticas.
+
+nome = 'João'
+idade = 37
+cemanos = (2022 - idade) + 100
+print(cemanos)
