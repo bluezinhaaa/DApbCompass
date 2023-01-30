1541c8f190c4e6de649218aa9a3c09de301cb635
Bruna Jacom <brunajacom@gmail.com>
2023-01-30 03:39:48 -0300

    e11 resolvido

---
+++ Sprint-3/Exercícios finais/E11.py
@@ -0,0 +1,8 @@
+#Escreva um programa que lê o conteúdo do arquivo texto arquivo_texto.txt e imprime o seu conteúdo.
+#Dica: leia a documentação da função open(...)
+
+
+ler_arquivo = open('arquivo_texto.txt', 'r')
+print(ler_arquivo.read())
+
+
