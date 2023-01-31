8633285d5fc9f84d8e2a9759bd1d8c8ba2127011
Bruna Jacom <brunajacom@gmail.com>
2023-01-31 05:53:08 -0300

    e25 resolvido

---
+++ Sprint-3/Exercícios finais/E25.py
@@ -0,0 +1,28 @@
+#Crie uma classe Avião que possua os atributos modelo, velocidade_maxima, cor e capacidade.
+#Defina o atributo cor de sua classe , de maneira que todas as instâncias de sua classe 
+# avião sejam da cor “azul”.
+#Após isso, a partir de entradas abaixo, instancie e armazene em uma lista 3 objetos da classe Avião.
+#Ao final, itere pela lista imprimindo cada um dos objetos no seguinte formato:
+#“O avião de modelo “x” possui uma velocidade máxima de “y”, capacidade para “z” 
+# passageiros e é da cor “w”.
+#Sendo x, y, z e w cada um dos atributos da classe “Avião”.
+#Valores de entrada:
+#modelo BOIENG456: velocidade máxima 1500 km/h: capacidade para 400 passageiros: Cor Azul
+#modelo Embraer Praetor 600: velocidade máxima 863km/h: capacidade para 14 passageiros: Cor Azul
+#modelo Antonov An-2: velocidade máxima de 258 Km/h: capacidade para 12 passageiros: Cor Azul
+
+class Aviao:
+        
+    def __init__(self, modelo, velocidade_maxima, capacidade, cor = 'Azul'):
+        self._modelo = modelo
+        self._velocidade_maxima = velocidade_maxima
+        self._cor = 'Azul'
+        self._capacidade = capacidade
+
+avioes = []
+avioes.append(Aviao("BOIENG456", 1500.0, 400))
+avioes.append(Aviao("Embraer Praetor 600", 863.0, 14))
+avioes.append(Aviao("Antonov An-2", 258.0, 12))
+
+for aviao in avioes:
+    print(f"O avião de modelo {aviao._modelo} possui uma velocidade máxima de {aviao._velocidade_maxima}, capacidade para {aviao._capacidade} passageiros e é da cor {aviao._cor}")
